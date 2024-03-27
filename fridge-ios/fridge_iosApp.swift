2eccdfc419c3b46e1c2d2d47a2f9b0c5a85bf6c7
leezahuor <leezahuor@gmail.com>
2024-03-27 15:05:10 -0700

    Initial Commit

---
+++ fridge-ios/fridge_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fridge_iosApp.swift
+//  fridge-ios
+//
+//  Created by Leeza Huor on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct fridge_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
