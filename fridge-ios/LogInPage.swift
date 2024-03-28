5dce0925adaa1acd368f2db2d4c010603d73b223
leezahuor <leezahuor@gmail.com>
2024-03-28 14:45:54 -0700

    Created new views

---
+++ fridge-ios/LogInPage.swift
@@ -0,0 +1,18 @@
+//
+//  LogInPage.swift
+//  fridge-ios
+//
+//  Created by Leeza Huor on 3/28/24.
+//
+
+import SwiftUI
+
+struct LogInPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInPage()
+}
