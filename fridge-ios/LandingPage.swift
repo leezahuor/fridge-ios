f8c92ca80a2ad4e596be50ee3eb96264d80e496d
leezahuor <leezahuor@gmail.com>
2024-04-11 13:44:32 -0700

    create initial layout

--- fridge-ios/LandingPage.swift
+++ fridge-ios/LandingPage.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct LandingPage: View {
     var body: some View {
-        Text("Landing Page")
+        VStack{
+            Spacer()
+            Text("Fridgventory")
+                .bold()
+            Spacer()
+            
+            Spacer()
+            Image("fridge-pic")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 200)
+            Spacer()
+            
+            Spacer()
+            Text("Enter Button")
+            Spacer()
+        }
     }
 }
 
