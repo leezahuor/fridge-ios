5302d61247a08ff43050744e0e613dba86bb5bd2
leezahuor <leezahuor@gmail.com>
2024-04-11 15:54:10 -0700

    created layout for log in/create profile page

--- fridge-ios/LogInCreateProfilePage.swift
+++ fridge-ios/LogInCreateProfilePage.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct LogInCreateProfilePage: View {
     var body: some View {
-        Text("Log In/Create Profile")
+        VStack{
+            Spacer()
+            Text("Log In")
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+                .padding(.bottom, 10.0)
+
+            Text("Create Profile")
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+            Spacer()
+        }
     }
 }
 
