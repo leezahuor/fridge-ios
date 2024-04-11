b8d52df21a6e0a0e2d9bb1c96b94c4afbf4c493b
leezahuor <leezahuor@gmail.com>
2024-04-11 16:43:24 -0700

    create login page layout

--- fridge-ios/LogInPage.swift
+++ fridge-ios/LogInPage.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct LogInPage: View {
     var body: some View {
-        Text("Log In Page")
+        VStack{
+            Text("Log In")
+                .font(.largeTitle)
+            Text("Username")
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                .padding(.bottom, 10.0)
+            Text("Password")
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                .padding(.bottom, 10.0)
+            Text("Submit")
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+                .padding(.bottom, 10.0)
+            Text("Back")
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+                .padding(.bottom, 10.0)
+        }
     }
 }
 
