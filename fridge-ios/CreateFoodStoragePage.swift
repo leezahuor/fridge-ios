d049e26c5df9a2331bbaa69f6ca010e3cb0d47a0
leezahuor <leezahuor@gmail.com>
2024-04-12 15:58:45 -0700

    create layout for food storage creation page

--- fridge-ios/CreateFoodStoragePage.swift
+++ fridge-ios/CreateFoodStoragePage.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct CreateFoodStoragePage: View {
     var body: some View {
-        Text("Create new food storage")
+        VStack{
+            Text("Create New Storage")
+                .font(.largeTitle)
+            Text("Name")
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                .padding(.bottom, 10.0)
+            HStack{
+                Text("Create")
+                    .padding()
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+                    .padding(.bottom, 10.0)
+                Text("Cancel")
+                    .padding()
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+                    .padding(.bottom, 10.0)
+            }
+        }
     }
 }
 
