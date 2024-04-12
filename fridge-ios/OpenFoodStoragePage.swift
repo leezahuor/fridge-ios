34841b915da7d68a18623f31d681459e25449457
leezahuor <leezahuor@gmail.com>
2024-04-12 15:53:35 -0700

    create layout for an opened food storage page

--- fridge-ios/OpenFoodStoragePage.swift
+++ fridge-ios/OpenFoodStoragePage.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct OpenFoodStoragePage: View {
     var body: some View {
-        Text("Inside Food Storage")
+        VStack{
+            HStack{
+                Text("< Back")
+                Spacer()
+                
+                Spacer()
+                Text("Edit")
+            }
+            
+            Text("Kitchen Freezer")
+                .font(.largeTitle)
+            
+            HStack {
+                Image("placeholder-food")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 50)
+                
+                Text("Ice Cream")
+                    .bold()
+                Text("(use by)")
+                Spacer()
+                Text("(exp date)")
+            }
+            .listRowSeparator(.hidden)
+            .listRowBackground(Color(.brown)
+                .opacity(0.1))
+            Spacer()
+        }
     }
 }
 
