02871f792a8647851f072cbed5dc80253f2142b5
leezahuor <leezahuor@gmail.com>
2024-04-15 16:37:54 -0700

    reworked login page and added navigation

--- fridge-ios/Core/Authentication/LogInPage.swift
+++ fridge-ios/Core/Authentication/LogInPage.swift
@@ -22,10 +22,49 @@ struct LogInPage: View {
                     .padding(.vertical, 32)
                 
                 // form fields
+                VStack(spacing: 24) {
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "name@example.com")
+                    .autocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecureField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
                 
                 // log in button
+                Button {
+                    print("Log user in...")
+                } label: {
+                    HStack {
+                        Text("Log In")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 40)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
                 Spacer()
+                
                 // create new profile button
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("No profile?")
+                        Text("Create one")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    }
+                    .font(.system(size: 14))
+                }
             }
         }
         
