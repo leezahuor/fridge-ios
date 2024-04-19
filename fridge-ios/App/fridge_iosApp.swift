2f4fcf32b281b1acff01d164a38569dbf93d47f2
leezahuor <leezahuor@gmail.com>
2024-04-19 16:40:49 -0700

    created environment object for view model

--- fridge-ios/App/fridge_iosApp.swift
+++ fridge-ios/App/fridge_iosApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct fridge_iosApp: App {
+    @StateObject var viewModel = AuthViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(viewModel)
         }
     }
 }
